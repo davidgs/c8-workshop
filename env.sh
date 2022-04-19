1ef3b390fa94ea9cac12f2ddbfede971689cbf3f
David G. Simmons <davidgs@users.noreply.github.com>
2022-04-19 18:07:56 -0400

    Delete env.sh

--- env.sh
+++
@@ -1,4 +0,0 @@
-export ZEEBE_ADDRESS='72eb571b-54c7-48e5-a44a-c226a4ed3556.bru-2.zeebe.camunda.io:443'
-export ZEEBE_CLIENT_ID='1D_yXoNJ_4-oWNMzzjMsk6kQd--PH7.g'
-export ZEEBE_CLIENT_SECRET='o4TP8FIEu~d.cKVOJSzsymkRe63J9c0ARcMZioTLq4ELhWjtT6vUBdE~PD6B55A3'
-export ZEEBE_AUTHORIZATION_SERVER_URL='https://login.cloud.camunda.io/oauth/token'
